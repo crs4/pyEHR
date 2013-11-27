498ddffae08d8b2f5b0059b31a7a2ca1b9ff00ce
Luca Lianas <lianas@crs4.it>
2013-11-27 18:08:53 +0100

    code refactory and added doctest

--- openehr/ehr/services/dbmanager/dbservices/wrappers.py
+++ openehr/ehr/services/dbmanager/dbservices/wrappers.py
@@ -3,15 +3,54 @@ from openehr.utils import decode_dict
 import time
 
 
+class RecordsFactory(object):
+
+    @staticmethod
+    def create_patient_record(document):
+        """
+        >>> doc = {
+        ...   u'creation_time': 1385569688.39202, 'last_update': 1385570688.39202,
+        ...   u'active': True, u'ehr_records': []
+        ... }
+        >>> record = RecordsFactory.create_patient_record(doc)
+        >>> print('%f %f %r %r' % (record.creation_time, record.last_update,
+        ...       record.active, record.ehr_records))
+        1385569688.392020 1385570688.392020 True []
+        """
+        document = decode_dict(document)
+        return PatientRecord(document['ehr_records'],
+                             document['creation_time'],
+                             document['last_update'],
+                             document['active'])
+
+    @staticmethod
+    def create_clinical_record(document):
+        """
+        >>> doc = {
+        ...   u'creation_time': 1385569688.39202, u'last_update': 1385570688.39202,
+        ...   u'active': True, u'ehr_data': {'field1': 'value1', 'field2': 'value2'}
+        ... }
+        >>> record = RecordsFactory.create_clinical_record(doc)
+        >>> print ('%f %f %r %r' % (record.creation_time, record.last_update,
+        ...        record.active, record.ehr_data))
+        1385569688.392020 1385570688.392020 True {'field2': 'value2', 'field1': 'value1'}
+        """
+        document = decode_dict(document)
+        return ClinicalRecord(document['ehr_data'],
+                              document['creation_time'],
+                              document['last_update'],
+                              document['active'])
+
+
 class Record(object):
 
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def __init__(self, creation_time):
+    def __init__(self, creation_time, last_update=None, active=True):
         self.creation_time = creation_time
-        self.last_update = creation_time
-        self.active = True
+        self.last_update = last_update or creation_time
+        self.active = active
 
     @abstractmethod
     def _to_document(self):
@@ -22,18 +61,13 @@ class Record(object):
         }
         return doc
 
-    @abstractmethod
-    def _from_document(self, doc):
-        doc = decode_dict(doc)
-        self.creation_time = doc['creation_time']
-        self.last_update = doc['last_update']
-        self.active = doc['active']
-
 
 class PatientRecord(Record):
 
-    def __init__(self, ehr_records=None):
-        super(PatientRecord, self).__init__(time.time())
+    def __init__(self, ehr_records=None, creation_time=None,
+                 last_update=None, active=True):
+        super(PatientRecord, self).__init__(creation_time or time.time(),
+                                            last_update, active)
         self.ehr_records = ehr_records or []
 
     def _to_document(self):
@@ -41,24 +75,16 @@ class PatientRecord(Record):
         doc['ehr_records'] = self.ehr_records
         return doc
 
-    def _from_document(self, doc):
-        doc = decode_dict(doc)
-        super(PatientRecord, self)._from_document(doc)
-        self.ehr_records = doc['ehr_records']
-
 
 class ClinicalRecord(Record):
 
-    def __init__(self, ehr_data):
-        super(ClinicalRecord, self).__init__(time.time())
+    def __init__(self, ehr_data, creation_time=None, last_update=None,
+                 active=True):
+        super(ClinicalRecord, self).__init__(creation_time or time.time(),
+                                             last_update, active)
         self.ehr_data = ehr_data
 
     def _to_document(self):
         doc = super(ClinicalRecord, self)._to_document()
         doc['ehr_data'] = self.ehr_data
         return doc
-
-    def _from_document(self, doc):
-        doc = decode_dict(doc)
-        super(ClinicalRecord, self)._from_document(doc)
-        doc['ehr_data'] = doc['ehr_data']
