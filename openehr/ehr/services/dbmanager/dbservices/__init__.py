bf4da8a4fddcbaa5e897d83939bd9049fca885c7
Luca Lianas <lianas@crs4.it>
2013-12-02 11:35:25 +0100

    bug fixes

--- openehr/ehr/services/dbmanager/dbservices/__init__.py
+++ openehr/ehr/services/dbmanager/dbservices/__init__.py
@@ -10,29 +10,29 @@ class DBServices(object):
                  passwd=None, logger=None):
         self.host = host
         self.database = database
-        self.patients_collection = patients_collection,
-        self.ehr_collection = ehr_collection,
-        self.port = port,
+        self.patients_collection = patients_collection
+        self.ehr_collection = ehr_collection
+        self.port = port
         self.user = user
         self.passwd = passwd
         self.logger = logger or get_logger('db_services')
 
     def save_patient_record(self, patient_record):
         """
-        >>> dbs = DBServices('localhost', 'test_databaset', 'test_patients_coll', 'test_ehr_coll')
+        >>> dbs = DBServices('localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = PatientRecord()
         >>> pat_rec = dbs.save_patient_record(pat_rec)
         >>> len(pat_rec.ehr_records) == 0
-        >>> pat_rec.record_id is None
-        >>> pat_rec.active
         True
+        >>> pat_rec.record_id is None
         False
+        >>> pat_rec.active
         True
         """
         with MongoDriver(self.host, self.database, self.patients_collection,
                          self.port, self.user, self.passwd, self.logger) as driver:
-            if patient_record.record.record_id is None:
-                patient_record.record.record_id = driver.add_record(patient_record._to_document())
+            if patient_record.record_id is None:
+                patient_record.record_id = driver.add_record(patient_record._to_document())
                 return patient_record
             else:
                 #TODO update or error?
