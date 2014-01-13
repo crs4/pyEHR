92fc90797916b1f3d94325ec0648f93a3d9140ae
Luca Lianas <lianas@crs4.it>
2014-01-13 15:19:04 +0100

    added documentation for DBServices class

--- openehr/ehr/services/dbmanager/dbservices/__init__.py
+++ openehr/ehr/services/dbmanager/dbservices/__init__.py
@@ -7,6 +7,21 @@ import time
 
 
 class DBServices(object):
+    """
+    This class exports all the services used to manipulate patients' and clinical data
+    stored in the database. The DBServices class acts as middleware to the specific driver.
+
+    :ivar driver: the type of driver that must be used
+    :ivar host: hostname of the server running the DB
+    :ivar user: (optional) username to access the DB
+    :ivar passwd: (optional) password to access the DB
+    :ivar port: (optional) port used to contact the DB
+    :ivar database: the name of the database where data are stored
+    :ivar patients_repository: (optional) repository where patients' data are stored
+    :ivar ehr_repository: (optional) repository where ehr data are stored
+    :ivar logger: logger for the DBServices class, if no logger is provided a new one
+      is created
+    """
 
     def __init__(self, driver, host, database, patients_repository=None,
                  ehr_repository=None, port=None, user=None, passwd=None, logger=None):
@@ -26,6 +41,12 @@ class DBServices(object):
 
     def save_patient(self, patient_record):
         """
+        Save a patient record to the DB.
+
+        :param patient_record: patient record that is going to be saved
+        :type patient_record: :class:`PatientRecord`
+        :return: a :class:`PatientRecord` object
+
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = PatientRecord()
         >>> pat_rec = dbs.save_patient(pat_rec)
@@ -44,6 +65,15 @@ class DBServices(object):
 
     def save_ehr_record(self, ehr_record, patient_record):
         """
+        Save a clinical record into the DB and link it to a patient record
+
+        :param ehr_record: EHR record that is going to be saved
+        :type ehr_record: :class:`ClinicalRecord`
+        :param patient_record: the reference :class:`PatientRecord` for the EHR record that
+          is going to be saved
+        :type patient_record: :class:`PatientRecord`
+        :return: the :class:`ClinicalRecord` and the :class:`PatientRecord`
+
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = PatientRecord()
         >>> pat_rec = dbs.save_patient(pat_rec)
@@ -64,14 +94,31 @@ class DBServices(object):
         return ehr_record, patient_record
 
     def add_ehr_record(self, patient_record, ehr_record):
+        """
+        Add an already saved :class:`ClinicalRecord` to the given :class:`PatientRecord`
+
+        :param patient_record: the reference :class:`PatientRecord`
+        :type patient_record: :class:`PatientRecord`
+        :param ehr_record: the existing :class:`ClinicalRecord` that is going to be added to the
+          patient record
+        :type ehr_record: :class:`ClinicalRecord`
+        :return: the updated :class:`PatientRecord`
+        """
         self._add_to_list(patient_record, 'ehr_records', ehr_record.record_id,
                           self.patients_repository)
         return self.get_patient(patient_record.record_id)
 
     def remove_ehr_record(self, ehr_record, patient_record):
         """
-        Remove an EHR record from a patient's records and delete it from the database.
-        Returns the EHR record without an ID so it can be assigned to a new patient (if needed).
+        Remove a :class:`ClinicalRecord` from a patient's records and delete
+        it from the database.
+
+        :param ehr_record: the :class:`ClinicalRecord` that will be deleted
+        :type ehr_record: :class:`ClinicalRecord`
+        :param patient_record: the reference :class:`PatientRecord`
+        :type patient_record: :class:`PatientRecord`
+        :return: the EHR record without an ID and the updated patient record
+        :rtype: :class:`ClinicalRecord`, :class:`PatientRecord`
 
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec_1 = dbs.save_patient(PatientRecord())
@@ -129,6 +176,20 @@ class DBServices(object):
 
     def get_patients(self, active_records_only=True, fetch_ehr_records=True,
                      fetch_hidden_ehr=False):
+        """
+        Get all patients from the DB.
+
+        :param active_records_only: if True fetch only active patient records, if False get all
+          patient records from the DB
+        :type active_records_only: boolean
+        :param fetch_ehr_records: if True fetch connected EHR records  as well, if False only EHR records'
+          IDs will be retrieved
+        :type fetch_ehr_records: boolean
+        :param fetch_hidden_ehr: if False only fetch active EHR records, if True fetch all EHR records
+          connected to the given patient record
+        :type fetch_hidden_ehr: boolean
+        :return: a list of :class:`PatientRecord` objects
+        """
         drf = self._get_drivers_factory(self.patients_repository)
         with drf.get_driver() as driver:
             if not active_records_only:
@@ -138,6 +199,18 @@ class DBServices(object):
                 return [self._fetch_patient_data_full(r) for r in self._get_active_records()]
 
     def get_patient(self, patient_id, fetch_ehr_records=True, fetch_hidden_ehr=False):
+        """
+        Load the `PatientRecord` that match the given ID from the DB.
+
+        :param patient_id: the ID of the record
+        :param fetch_ehr_records: if True fetch connected EHR records  as well, if False only EHR records'
+          IDs will be retrieved
+        :type fetch_ehr_records: boolean
+        :param fetch_hidden_ehr: if False only fetch active EHR records, if True fetch all EHR records
+          connected to the given patient record
+        :type fetch_hidden_ehr: boolean
+        :return: the :class:`PatientRecord` matching the given ID or None if no matching record was found
+        """
         drf = self._get_drivers_factory(self.patients_repository)
         with drf.get_driver() as driver:
             return self._fetch_patient_data_full(driver.get_record_by_id(patient_id),
@@ -145,6 +218,13 @@ class DBServices(object):
 
     def load_ehr_records(self, patient):
         """
+        Load all :class:`ClinicalRecord` objects connected to the given :class:`PatientRecord` object
+
+        :param patient: the patient record object
+        :type patient: :class:`PatientRecord`
+        :return: the :class:`PatientRecord` object with loaded :class:`ClinicalRecord`
+        :type: :class;`PatientRecord`
+
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = dbs.save_patient(PatientRecord(record_id='PATIENT_01'))
         >>> for x in xrange(5):
@@ -175,6 +255,13 @@ class DBServices(object):
 
     def hide_patient(self, patient):
         """
+        Hide a ;class:`PatientRecord` object
+
+        :param patient: the patient record that is going to be hidden
+        :type param: :class:`PatientRecord`
+        :return: the patient record
+        :rtype: :class:`PatientRecord`
+
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = dbs.save_patient(PatientRecord())
         >>> for x in xrange(5):
@@ -207,6 +294,13 @@ class DBServices(object):
 
     def hide_ehr_record(self, ehr_record):
         """
+        Hide a :class:`ClinicalRecord` object
+
+        :param ehr_record: the clinical record that is going to be hidden
+        :type ehr_record: ;class:`ClinicalRecord`
+        :return: the clinical record
+        :rtype: :class:`ClinicalRecord`
+
         >>> dbs = DBServices('mongodb', 'localhost', 'test_database', 'test_patients_coll', 'test_ehr_coll')
         >>> pat_rec = dbs.save_patient(PatientRecord())
         >>> for x in xrange(5):
@@ -241,6 +335,18 @@ class DBServices(object):
         return rec
 
     def delete_patient(self, patient, cascade_delete=False):
+        """
+        Delete a patient from the DB. A patient record can be deleted only if it has no clinical record connected
+        or if the *cascade_delete* option is set to True. If clinical records are still connected and *cascade_delete*
+        option is set to False, a :class:`CascadeDeleteError` exception will be thrown.
+
+        :param patient: the patient record that is going to be deleted
+        :type patient: :class:`PatientRecord`
+        :param cascade_delete: if True connected `ClinicalRecord` objects will be deleted as well
+        :type cascade_delete: boolean
+        :raise: :class:`CascadeDeleteError` if a record with connected clinical record is going to be deleted and
+          cascade_delete option is False
+        """
         def reload_patient(patient):
             return self.get_patient(patient.record_id, fetch_ehr_records=False,
                                     fetch_hidden_ehr=True)
