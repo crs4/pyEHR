2f0b84d9ef10b48f25f2d9b10d67162fde25d21b
Luca Lianas <lianas@crs4.it>
2014-03-12 15:22:45 +0100

    implemented patient\ehr record delete test

--- test/services/dbservice.py
+++ test/services/dbservice.py
@@ -141,11 +141,113 @@ class TestDBService(unittest.TestCase):
         r, c = self._send_request(delete_patient_path, delete_patient_details)
         self.assertEqual(r.status, 200)
 
+    def test_delete_patient(self):
+        add_patient_path = '/patient/add'
+        add_ehr_path = '/ehr/add'
+        delete_patient_path = '/patient/delete'
+        #check for mandatory fields
+        r, c = self._send_request(delete_patient_path, {})
+        self.assertEqual(r.status, 400)
+        # add a patient
+        patient_details = self._build_add_patient_request(patient_id='TEST_PATIENT')
+        r, c = self._send_request(add_patient_path, patient_details)
+        self.assertEqual(r.status, 200)
+        # try to delete a non existing patient
+        patient_delete_request = self._build_delete_patient_request()
+        r, c = self._send_request(delete_patient_path, patient_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertFalse(c['SUCCESS'])
+        # delete patient
+        patient_delete_request = self._build_delete_patient_request(patient_id='TEST_PATIENT')
+        r, c = self._send_request(delete_patient_path, patient_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertTrue(c['SUCCESS'])
+        # create the patient again ...
+        r, c = self._send_request(add_patient_path, patient_details)
+        self.assertEqual(r.status, 200)
+        # ... and add an EHR record
+        ehr_record = {
+            'archetype': 'openEHR.TEST-EVALUATION.v1',
+            'ehr_data': {'k1': 'v1', 'k2': 'v2'}
+        }
+        ehr_add_request = self._build_add_ehr_request(patient_id='TEST_PATIENT',
+                                                      ehr_record=ehr_record)
+        r, c = self._send_request(add_ehr_path, ehr_add_request)
+        self.assertEqual(r.status, 200)
+        # try to delete patient record without the cascade flag enabled
+        r, c = self._send_request(delete_patient_path, patient_delete_request)
+        self.assertEqual(r.status, 500)
+        # enable the cascade delete and retry
+        patient_delete_request = self._build_delete_patient_request(patient_id='TEST_PATIENT',
+                                                                    cascade_delete=True)
+        r, c = self._send_request(delete_patient_path, patient_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertTrue(c['SUCCESS'])
+
+    def test_delete_ehr_record(self):
+        add_patient_path = '/patient/add'
+        add_ehr_path = '/ehr/add'
+        delete_patient_path = '/patient/delete'
+        delete_ehr_path = '/ehr/delete'
+        # create patient
+        patient_details = self._build_add_patient_request(patient_id='TEST_PATIENT')
+        r, c = self._send_request(add_patient_path, patient_details)
+        self.assertEqual(r.status, 200)
+        # add an EHR record
+        ehr_record = {
+            'archetype': 'openEHR.TEST-EVALUATION.v1',
+            'ehr_data': {'k1': 'v1', 'k2': 'v2'}
+        }
+        ehr_add_request = self._build_add_ehr_request(patient_id='TEST_PATIENT',
+                                                      ehr_record=ehr_record)
+        r, c = self._send_request(add_ehr_path, ehr_add_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        # get EHR record ID (will be used to deletere the record itself)
+        ehr_record_id = c['RECORD']['record_id']
+        self.assertTrue(c['SUCCESS'])
+        # check for mandatory fields
+        r, c = self._send_request(delete_ehr_path, {})
+        self.assertEqual(r.status, 400)
+        ehr_delete_request = self._build_delete_ehr_request(patient_id='TEST_PATIENT')
+        r, c = self._send_request(delete_ehr_path, ehr_delete_request)
+        self.assertEqual(r.status, 400)
+        # try to delete an EHR record connected to a non existing individual
+        ehr_delete_request = self._build_delete_ehr_request(ehr_record_id=ehr_record_id)
+        r, c = self._send_request(delete_ehr_path, ehr_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertFalse(c['SUCCESS'])
+        # now try to delete a non existing EHR record
+        ehr_delete_request = self._build_delete_ehr_request(patient_id='TEST_PATIENT',
+                                                            ehr_record_id='THIS_IS_NOT_AN_EHR_ID')
+        r, c = self._send_request(delete_ehr_path, ehr_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertFalse(c['SUCCESS'])
+        # delete the EHR record
+        ehr_delete_request = self._build_delete_ehr_request(patient_id='TEST_PATIENT',
+                                                            ehr_record_id=ehr_record_id)
+        r, c = self._send_request(delete_ehr_path, ehr_delete_request)
+        self.assertEqual(r.status, 200)
+        c = decode_dict(json.loads(c))
+        self.assertTrue(c['SUCCESS'])
+        # delete patient record
+        patient_delete_request = self._build_delete_patient_request(patient_id='TEST_PATIENT',
+                                                                    cascade_delete=True)
+        r, c = self._send_request(delete_patient_path, patient_delete_request)
+        self.assertTrue(r.status, 200)
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestDBService('test_add_patient'))
     suite.addTest(TestDBService('test_add_ehr_record'))
+    suite.addTest(TestDBService('test_delete_patient'))
+    suite.addTest(TestDBService('test_delete_ehr_record'))
     return suite
 
 if __name__ == '__main__':
