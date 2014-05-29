0fd55d0ce512b17baf916e01b4bf43dd9fcfad30
Luca Lianas <lianas@crs4.it>
2014-05-29 12:25:56 +0200

    unittest for index service

---
+++ test/pyehr/ehr/services/dbmanager/dbservices/test_index_service.py
@@ -0,0 +1,40 @@
+import unittest, sys, os
+from lxml import etree
+from pyehr.ehr.services.dbmanager.dbservices.index_service import IndexService
+from pyehr.ehr.services.dbmanager.dbservices.wrappers import PatientRecord,\
+    ClinicalRecord, ArchetypeInstance
+from pyehr.utils.services import get_service_configuration
+
+CONF_FILE = os.getenv('SERVICE_CONFIG_FILE')
+
+
+class TestIndexService(unittest.TestCase):
+
+    def __init__(self, label):
+        super(TestIndexService, self).__init__(label)
+
+    def test_structure_simple(self):
+        ehr_record = {
+            'archetype': 'test-openehr-OBSERVATION.test01.v1',
+            'data': {
+                'at0001': {
+                    'archetype': 'test-openehr-OBSERVATION.test02.v1',
+                    'data': {}
+                }
+            }
+        }
+        expected_structure = '<archetype class="test-openehr-OBSERVATION.test01.v1">' + \
+            '<archetype class="test-openehr-OBSERVATION.test02.v1"/>' + \
+            '</archetype>'
+        ehr_structure = etree.tostring(IndexService.get_structure(ehr_record))
+        self.assertEqual(ehr_structure, expected_structure)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestIndexService('test_structure_simple'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
