727b9ae1273375fffadc6a6cfc13755bc63fd587
Luca Lianas <lianas@crs4.it>
2014-05-29 14:12:58 +0200

    added more tests

--- test/pyehr/ehr/services/dbmanager/dbservices/test_index_service.py
+++ test/pyehr/ehr/services/dbmanager/dbservices/test_index_service.py
@@ -1,4 +1,4 @@
-import unittest, sys, os
+import unittest, os
 from lxml import etree
 from pyehr.ehr.services.dbmanager.dbservices.index_service import IndexService
 from pyehr.ehr.services.dbmanager.dbservices.wrappers import PatientRecord,\
@@ -29,10 +29,59 @@ class TestIndexService(unittest.TestCase):
         ehr_structure = etree.tostring(IndexService.get_structure(ehr_record))
         self.assertEqual(ehr_structure, expected_structure)
 
+    def test_structure_dict(self):
+        ehr_record = {
+            'archetype': 'test-openehr-OBSERVATION.test01.v1',
+            'data': {
+                'cluster': {
+                    'at0001': {
+                        'archetype': 'test-openehr-OBSERVATION.test02.v1',
+                        'data': {}
+                    },
+                    'at0002': {
+                        'archetype': 'test-openehr-OBSERVATION.test03.v1',
+                        'data': {}
+                    },
+                    'at0003': 'foobar'
+                }
+            }
+        }
+        expected_structure = '<archetype class="test-openehr-OBSERVATION.test01.v1">' + \
+            '<archetype class="test-openehr-OBSERVATION.test02.v1"/>' + \
+            '<archetype class="test-openehr-OBSERVATION.test03.v1"/>' + \
+            '</archetype>'
+        ehr_structure = etree.tostring(IndexService.get_structure(ehr_record))
+        self.assertEqual(ehr_structure, expected_structure)
+
+    def test_structure_list(self):
+        ehr_record = {
+            'archetype': 'test-openehr-OBSERVATION.test01.v1',
+            'data': {
+                'at0001': [
+                    {
+                        'archetype': 'test-openehr-OBSERVATION.test02.v1',
+                        'data': {}
+                    },
+                    {
+                        'archetype': 'test-openehr-OBSERVATION.test03.v1',
+                        'data': {}
+                    }
+                ]
+            }
+        }
+        expected_structure = '<archetype class="test-openehr-OBSERVATION.test01.v1">' + \
+            '<archetype class="test-openehr-OBSERVATION.test02.v1"/>' + \
+            '<archetype class="test-openehr-OBSERVATION.test03.v1"/>' + \
+            '</archetype>'
+        ehr_structure = etree.tostring(IndexService.get_structure(ehr_record))
+        self.assertEqual(ehr_structure, expected_structure)
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestIndexService('test_structure_simple'))
+    suite.addTest(TestIndexService('test_structure_dict'))
+    suite.addTest(TestIndexService('test_structure_list'))
     return suite
 
 if __name__ == '__main__':
