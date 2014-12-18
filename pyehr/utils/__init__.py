b5cd72520758bccf82c363c32a92873bf54d942c
Luca Lianas <lianas@crs4.it>
2014-12-18 16:54:41 +0100

    default log level is now WARNING
    Signed-off-by: Luca Lianas <lianas@crs4.it>

--- pyehr/utils/__init__.py
+++ pyehr/utils/__init__.py
@@ -5,7 +5,7 @@ LOG_FORMAT = '%(asctime)s|%(levelname)-8s|%(message)s'
 LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
 
-def get_logger(name, log_level='INFO', log_file=None, mode='a'):
+def get_logger(name, log_level='WARNING', log_file=None, mode='a'):
     logger = logging.getLogger(name)
     if not isinstance(log_level, int):
         try:
