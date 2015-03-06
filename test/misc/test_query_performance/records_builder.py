66629ea45c3f40d5ac5021e726d3033433fd697c
Luca Lianas <lianas@crs4.it>
2015-03-06 10:20:37 +0100

    dataset creation function, still need some fixes (safety commit)

--- test/misc/test_query_performance/records_builder.py
+++ test/misc/test_query_performance/records_builder.py
@@ -82,6 +82,74 @@ def build_patient_dataset(patient_label, dataset_conf, structures, archetypes_di
     return {patient_label: records}
 
 
+def resolve_percentage(percentage, total):
+    return round((percentage * total) / 100.)
+
+
+def build_dataset(dataset_conf):
+    """
+    {
+      "records_count": 1000,
+      "patients_count": 10,
+      "records_distribution": {
+        2: {
+          "matches": 10, // percentage on records_count
+          "hits": 5,     // percentage on records_count
+          "hit_condition": {"blood_pressure": {"systolic": (randint, 10, 20)}},
+          "no_hit_condition": {"blood_pressure": {"systolic": (randint, 200, 250)}}
+        }
+      }
+    }
+    """
+    patients_map = {}
+    # get records count
+    for x in xrange(dataset_conf['patients_count']):
+        plabel = 'PATIENT_%d' % x
+        patients_map[plabel] = {
+            'records_count': int(round(dataset_conf['records_count'] / float(dataset_conf['patients_count']))),
+            'records_distribution': {},
+            'hits': {}
+        }
+    if dataset_conf['records_count'] % dataset_conf['patients_count'] != 0:
+        for x in xrange(dataset_conf['records_count'] % dataset_conf['patients_count']):
+            patients_map[patients_map.keys()[x]]['records_count'] += 1
+    # get hits distribution
+    previous_level_hits = 0
+    previous_level_matches = 0
+    for level, conf in sorted(dataset_conf['records_distribution'].iteritems(), reverse=True):
+        hits_count = (resolve_percentage(conf['hits'], dataset_conf['records_count'])) - previous_level_hits
+        if hits_count < 0:
+            hits_count = 0
+        for x in xrange(int(hits_count)):
+            hmap = patients_map[choice(patients_map.keys())]
+            hmap['hits'].setdefault(level, {})
+            hmap['hits'][level].setdefault('hits_count', 0)
+            hmap['records_distribution'].setdefault(level, 0)
+            hmap['hits'][level]['hits_count'] += 1
+            hmap['records_distribution'][level] += 1
+            hmap['hits'][level].setdefault('hit_condition', conf['hit_condition'])
+            hmap['hits'][level].setdefault('no_hit_condition', conf['no_hit_condition'])
+        previous_level_hits += hits_count
+        # get matches  distribution
+        matches_count = (resolve_percentage(conf['matches'], dataset_conf['records_count'])) -\
+                        previous_level_matches - previous_level_hits
+        if matches_count < 0:
+            matches_count = 0
+        for x in xrange(int(matches_count)):
+            patient_key = choice(patients_map.keys())
+            pmap = patients_map[patient_key]['records_distribution']
+            patients_map[patient_key].setdefault('hits', {})
+            pmap.setdefault(level, 0)
+            pmap[level] += 1
+            # set NO MATCH condition for current level
+            if level not in patients_map[patient_key]['hits']:
+                patients_map[patient_key]['hits'].setdefault(
+                    level, {'no_hit_condition': conf['no_hit_condition']}
+                )
+        previous_level_matches += matches_count
+    return patients_map
+
+
 def contains_archetype(structure_description, archetype_label):
     element_label = archetype_label[0]
     if isinstance(structure_description, dict):
